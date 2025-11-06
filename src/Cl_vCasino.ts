263f5d704b47259e69df667e636bc9bdcd98be04
Moises-Torrealba <moisesdavidtor@gmail.com>
2025-11-05 21:04:45 -0400

    ajuste correcto

--- src/Cl_vCasino.ts
+++ src/Cl_vCasino.ts
@@ -23,7 +23,7 @@ export default class Cl_vCasino extends Cl_vGeneral {
       this.divJugadoresRegistrados.innerHTML += `<tr>
             <td>${jugador.nombre}</td>
             <td>${jugador.tragamonedas}</td>
-            <td>bs ${jugador.premio}</td>
+            <td>${jugador.premio} bs</td>
         </tr>`;
     });
   }
